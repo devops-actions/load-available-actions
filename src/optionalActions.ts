13491766e650b4f1664f4d38f84adcc5299fcd22
Vineeth Puthiya Veettil <vineeth.puthiya.veettil@asml.com>
2023-01-10 15:50:53 +0100

    Fix issues with fetching the readme files

--- src/optionalActions.ts
+++ src/optionalActions.ts
@@ -1,19 +1,19 @@
-import { Repository } from './main'
 import {Octokit} from 'octokit'
 import * as core from '@actions/core'
 
 export async function getReadmeContent(
   client: Octokit,
-  repo: Repository
+  repo: string,
+  owner: string
 ): Promise<string | undefined> {
   try {
     const {data: readme} = await client.rest.repos.getContent({
-      owner: repo.owner,
-      repo: repo.name,
+      owner,
+      repo,
       path: 'README.md'
     })
     return readme.content
   } catch (error) {
-    core.debug(`No readme file found in repository: ${repo.name}`)
+    core.debug(`No readme file found in repository: ${repo}`)
   }
 }
