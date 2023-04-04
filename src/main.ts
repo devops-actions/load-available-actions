863fdfb31a63f1108ed77b3ea89f2c6ac407c97c
Rob Bos <rajbos@users.noreply.github.com>
2023-04-04 13:30:55 +0000

    cut off the extra slash

--- src/main.ts
+++ src/main.ts
@@ -216,7 +216,7 @@ async function getAllActionsFromForkedRepos(
     for (let index = 0; index < actionFiles.length - 1; index++) {
       core.debug(`Found action file [${actionFiles[index]}] in repo [${repoName}]`)
       // remove the actions/$repopath
-      const actionFile = actionFiles[index].substring((`actions/${repoName}`).length)
+      const actionFile = actionFiles[index].substring((`actions/${repoName}/`).length)
       core.debug(`Found action file [${actionFile}] in repo [${repoName}]`)
       const action = new Content()
       action.repo = repoName
