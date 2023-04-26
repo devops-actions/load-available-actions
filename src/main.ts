6393aa3143105a83f4627ccde76db4b212590e81
Jakub Niemczyk <jakub.niemczyk@asml.com>
2023-04-26 10:18:15 +0200

    main: Fixed

--- src/main.ts
+++ src/main.ts
@@ -177,13 +177,13 @@ async function getAllActions(
     organization,
     isEnterpriseServer
   )
-  const allActionableDockerFiles = await getActionableDockerFiles(
-    client,
-    username,
-    organization,
-    isEnterpriseServer
-  )
-  core.info(`all actionable docker files: ${allActionableDockerFiles}`)
+  // const allActionableDockerFiles = await getActionableDockerFiles(
+  //   client,
+  //   username,
+  //   organization,
+  //   isEnterpriseServer
+  // )
+  // core.info(`all actionable docker files: ${allActionableDockerFiles}`)
   actions = actions.concat(forkedActions)
   return actions
 }
